332d180b8073b863dc77b38132216b91ba0b80dc
Marko Krstulovic <mk366d@outlook.com>
2024-03-12 16:09:21 -0400

    Completed class activitie for March 12, 2024

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,23 @@
 const express = require('express')
 const router = express.Router();
+
+router.get('/usernames', (req, res) => {
+  let usernames = req.users.map(function(user) {
+    return {id: user.id, username: user.username};
+  });
+  res.send(usernames);
+});
+
+router.get('/username/:name', (req, res) => {
+  let name = req.params.name;
+  let users_with_name = req.users.filter(function(user) {
+    return user.username === name;
+  });
+  if (users_with_name.length === 0) {
+    console.log("Error: None found");
+  } else {
+    res.send(users_with_name);
+  }
+});
+
+module.exports = router;
